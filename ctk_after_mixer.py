1f5b86327cde1468879ec21b1f65fe2fb0319c40
alekseewatatianawork73 <153320481+alekseewatatianawork73@users.noreply.github.com>
2026-03-31 14:54:37 +0300

    Update ctk_after_mixer.py

--- ctk_after_mixer.py
+++ ctk_after_mixer.py
@@ -17,10 +17,10 @@ def press1():
     # при нажатии на кнопку один раз воспроизводится короткий звук
     mus_btn.play(0)
     # удаляем текстовую метку
-    lbl1.grid_forget()
+    lbl.grid_forget()
     # ждём 3 секунды (3000 мс) и снова возвращаем текстовую метку на экран
     # используем lambda-функцию, потому что выполняется всего одно действие - размещение при помощи grid()
-    root.after(3000, lambda: lbl1.grid(row=1, column=3, padx=10, pady=10))
+    root.after(3000, lambda: lbl.grid(row=1, column=3, padx=10, pady=10))
     # синтаксис: [окно, в котором выполняются действия].afler([время ожидания в мс],[функция, которую необходимо выполнить])
     # 1 секунда = 1000 миллисекунд
 
@@ -41,6 +41,26 @@ def press3():
     mixer.music.stop()
 
 
+# функция для кнопки "Мигающая надпись"
+def press4():
+    global cur_color, after_id
+    # меняем цвет надписи
+    if cur_color == 'black':
+        cur_color = 'red'
+    else:
+        cur_color = 'black'
+    lbl.configure(text_color=cur_color)
+    # повторяем эту же функцию через полсекунды (500 миллисекунд)
+    # сохраняем в переменную, чтобы позже можно было отменить мигание надписи
+    after_id = root.after(500, press4)
+
+
+# функция для кнопки "Немигающая надпись"
+def press5():
+    global after_id
+    root.after_cancel(after_id)  # отменяем повторяющиеся мигания
+
+
 # основная программа
 root = ctk.CTk()
 root.title("Моё приложение")
@@ -53,24 +73,38 @@ for i in range(rows):
 for i in range(columns):
     root.columnconfigure(index=i, weight=1)
 
-my_font = ctk.CTkFont(family='Arial', size=25, weight='bold')
+my_font = ctk.CTkFont(family='Arial', size=20, weight='bold')
+lbl_font = ctk.CTkFont(family='Arial', size=30, weight='bold')
 
-# создаём виджеты
-lbl1 = ctk.CTkLabel(master=root)
-lbl1.configure(text=f'Text', font=my_font)
-lbl1.grid(row=1, column=3, padx=10, pady=10, sticky='nsew')
+# создаём надпись в окне
+cur_color = 'black'  # текущий цвет текста
+lbl = ctk.CTkLabel(master=root)
+lbl.configure(text=f'Мой текст', text_color=cur_color, font=lbl_font)
 
+# создаём кнопки
 btn1 = ctk.CTkButton(master=root)
 btn1.configure(text="Удалить текст на 3 секунды", font=my_font, command=press1)
-btn1.grid(row=4, column=3, padx=20, pady=20, sticky="nsew")
 
 btn2 = ctk.CTkButton(master=root)
 btn2.configure(text="Включить звук", font=my_font, command=press2)
-btn2.grid(row=5, column=2, padx=20, pady=20, sticky="nsew")
 
 btn3 = ctk.CTkButton(master=root)
 btn3.configure(text="Выключить звук", font=my_font, command=press3)
-btn3.grid(row=5, column=4, padx=20, pady=20, sticky="nsew")
+
+after_id = None  # переменная для выполнения мигания надписи
+btn4 = ctk.CTkButton(master=root)
+btn4.configure(text="Мигающая надпись", font=my_font, command=press4)
+
+btn5 = ctk.CTkButton(master=root)
+btn5.configure(text="Немигающая надпись", font=my_font, command=press5)
+
+# размещение элементов в окне
+lbl.grid(row=1, column=3, padx=10, pady=10, sticky='nsew')
+btn1.grid(row=2, column=3, padx=20, pady=20, sticky="nsew")
+btn2.grid(row=3, column=2, padx=20, pady=20, sticky="nsew")
+btn3.grid(row=3, column=4, padx=20, pady=20, sticky="nsew")
+btn4.grid(row=2, column=2, padx=20, pady=20, sticky="nsew")
+btn5.grid(row=2, column=4, padx=20, pady=20, sticky="nsew")
 
 # бесконечный цикл для отображения окна приложения
 root.mainloop()
